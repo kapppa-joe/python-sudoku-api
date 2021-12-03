c4be406e31c6807b4495b7791900cbbbadca1bdf
Joe Fong <kapppa.joe@gmail.com>
2021-12-03 21:36:56 +0000

    remove unneeded comments in seed function

--- db/seeds.py
+++ db/seeds.py
@@ -1,18 +1,9 @@
 # This file should contain records you want created when you run flask db seed.
 #
-# Example:
-# from yourapp.models import User
 
-
-from sudoku_api.models.Puzzle import Puzzle, generate_puzzles
+from sudoku_api.models.Puzzle import generate_puzzles
 from app import db
 puzzles = generate_puzzles()
 for p in puzzles:
     db.session.add(p)
 db.session.commit()
-
-# initial_user = {
-#     'username': 'superadmin'
-# }
-# if User.find_by_username(initial_user['username']) is None:
-#     User(**initial_user).save()
