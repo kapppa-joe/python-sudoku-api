2956d97539b3f7bc9b42aac526bf7c4d4bdfcb44
Joe Fong <kapppa.joe@gmail.com>
2021-12-04 21:17:53 +0000

    remove unneeded comments

--- sudoku_api/__init__.py
+++ sudoku_api/__init__.py
@@ -27,26 +27,3 @@ def create_app(test_config=None):
     api.add_resource(Solver, '/solver')
     api.add_resource(Puzzle, '/puzzles', '/puzzles/<int:puzzle_id>')
     return app
-
-# def create_app(test_config=None):
-#     # from sudoku_api.models.Puzzles import db
-#     app = Flask(__name__)
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#     if test_config is None:
-#         app.config.from_pyfile('config.py', silent=False)
-#     else:
-#         app.config.from_mapping(test_config)
-
-#     # db.init_app(app)
-#     return app
-
-
-# def init_db():
-#     from sudoku_api.models.Puzzles import db
-#     db.create_all()
-
-
-# if __name__ == '__main__':
-#     app = create_app()
-#     app.run(debug=True)
