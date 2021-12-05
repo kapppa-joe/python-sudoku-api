fe5d3c1eb18f69aeb115f81e6de955977cae8403
Joe Fong <kapppa.joe@gmail.com>
2021-12-05 21:26:41 +0000

    fix problem in procfile and app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from sudoku_api import create_app, db
+from sudoku_api import create_app
 
+app = create_app()
 if __name__ == '__main__':
-    app = create_app()
     app.run(threaded=True, port=5000)
